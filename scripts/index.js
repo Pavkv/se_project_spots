c62179c78635394ba65188210bf60149c8c2d551
Pavel Zobov <pavelzobov123@gmail.com>
2025-01-21 18:12:24 -0500

    Project 4. Stage 1. Added new pictures, added new script with picture objects, attached the script to index.html

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = {
+    1: {
+        name: "Trevi Fountain",
+        link: "../images/photo1-Trevi-Fountain.jpg"
+    },
+    2: {
+        name: "Fiorenze",
+        link: "../images/photo2-Fiorenze.jpg"
+    },
+    3: {
+        name: "Street View",
+        link: "../images/photo3-Street-View.jpg"
+    },
+    4: {
+        name: "River View",
+        link: "../images/photo4-River-View.jpg"
+    },
+    5: {
+        name: "Cafe",
+        link: "../images/photo5-Cafe.jpg"
+    },
+    6: {
+        name: "Riviera",
+        link: "../images/photo6-Riviera.jpg"
+    }
+}
