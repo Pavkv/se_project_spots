7499398be049e90e85e745f1b4ae3e73eabc9fad
Pavel Zobov <pavelzobov123@gmail.com>
2025-03-02 20:29:54 -0500

    fix: Update the project according to the best coding practices

---
+++ components/FormValidation.js
@@ -0,0 +1,66 @@
+export default class FormValidation {
+    constructor(formElement, formSelectors) {
+        this._formElement = formElement;
+        this._formSelectors = formSelectors;
+        this._inputList = Array.from(formElement.querySelectorAll(formSelectors.inputSelector));
+        this._buttonElement = formElement.querySelector(formSelectors.submitButtonSelector);
+    }
+
+    _showInputError = (inputElement, errorElement) => {
+        inputElement.classList.add(this._formSelectors.inputErrorClass);
+        errorElement.classList.add(this._formSelectors.errorClass);
+        errorElement.textContent = inputElement.validationMessage;
+    };
+
+    _hideInputError = (inputElement, errorElement) => {
+        inputElement.classList.remove(this._formSelectors.inputErrorClass);
+        errorElement.classList.remove(this._formSelectors.errorClass);
+        errorElement.textContent = "";
+    };
+
+    _checkInputValidity = (inputElement, errorElement) => {
+        inputElement.validity.valid ? this._hideInputError(inputElement, errorElement) :
+            this._showInputError(inputElement, errorElement);
+    };
+
+    _hasInvalidInput = () => this._inputList.some(inputElement => !inputElement.validity.valid);
+
+    _toggleButtonState = () => {
+        const isDisabled = this._hasInvalidInput();
+        this._buttonElement.disabled = isDisabled;
+        this._buttonElement.classList.toggle(this._formSelectors.inactiveButtonClass, isDisabled);
+    };
+
+    _disableButton = () => {
+        this._buttonElement.disabled = true;
+        this._buttonElement.classList.add(this._formSelectors.inactiveButtonClass);
+    }
+
+    _resetValidation = () => {
+        this._inputList.forEach(inputElement => {
+            const errorElement = inputElement.parentElement.querySelector(this._formSelectors.errorSelector);
+            this._hideInputError(inputElement, errorElement);
+        });
+    };
+
+    _setEventListeners = () => {
+        this._formElement.addEventListener("reset", this._disableButton);
+        this._inputList.forEach(inputElement => {
+            const errorElement = inputElement.parentElement.querySelector(this._formSelectors.errorSelector);
+            const validateInput = () => {
+                this._checkInputValidity(inputElement, errorElement);
+                this._toggleButtonState();
+            };
+            inputElement.addEventListener("input", validateInput);
+            inputElement.addEventListener("click", validateInput);
+        });
+    };
+
+    enableValidation = () => {
+        this._formElement.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+            this._resetValidation();
+        });
+        this._setEventListeners();
+    };
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,206 +1,59 @@
-const shortLink =
-    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/";
-const initialCards = [
+import * as constants from "../utils/constants.js";
+import Popup from "../components/Popup.js";
+import Section from "../components/Section.js";
+import Card from "../components/Card.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import FormValidation from "../components/FormValidation.js";
+
+const fullSizePopup = new Popup(constants.fullImageSelectors.fullImagePopup);
+fullSizePopup.setEventListeners();
+
+const cardList = new Section(
     {
-        name: "Val Thorens",
-        src: shortLink + "1-photo-by-moritz-feldmann-from-pexels.jpg",
-        alt: "Ski resort view",
+        items: constants.initialCards,
+        renderer: (item) => new Card(item, constants.cardSelectors, {
+            fullSize: (data) => {
+                const { fullImageImgElement, fullImageText } = constants.fullImageSelectors;
+                document.querySelector(fullImageImgElement).src = data.src;
+                document.querySelector(fullImageImgElement).alt = data.alt;
+                document.querySelector(fullImageText).textContent = data.name;
+                fullSizePopup.togglePopup();
+            }
+        }).getCard()
     },
-    {
-        name: "Restaurant terrace",
-        src: shortLink + "2-photo-by-ceiline-from-pexels.jpg",
-        alt: "Restaurant view",
+    constants.photoList
+);
+cardList.renderItems();
+
+const updateProfileForm = new PopupWithForm(constants.editSelectors.editPopup, {
+    fill: (index) => {
+        return constants.profileSelectors[index].textContent.trim();
     },
-    {
-        name: "An outdoor cafe",
-        src: shortLink + "3-photo-by-tubanur-dogan-from-pexels.jpg",
-        alt: "An outdoor cafe view",
-    },
-    {
-        name: "A very long bridge, over the forest and through the trees",
-        src: shortLink + "4-photo-by-maurice-laschet-from-pexels.jpg",
-        alt: "Forest wooden bridge view",
-    },
-    {
-        name: "Tunnel with morning light",
-        src: shortLink + "5-photo-by-van-anh-nguyen-from-pexels.jpg",
-        alt: "Long hallway view",
-    },
-    {
-        name: "Mountain house",
-        src: shortLink + "6-photo-by-moritz-feldmann-from-pexels.jpg",
-        alt: "Winter cabin view",
-    },
-    {
-        name: "Golden Gate Bridge",
-        src: shortLink + "7-photo-by-griffin-wooldridge-from-pexels.jpg",
-        alt: "Golden gate bridge view",
-    },
-];
-
-const profileForm = document.forms["profile-form"];
-const newPostForm = document.forms["new-post-form"];
-const photoList = document.querySelector(".photos__list");
-
-const profileSelectors = {
-    editModal: document.querySelector("#edit-modal"),
-    editButton: document.querySelector(".profile__edit-profile"),
-    editName: profileForm.querySelector("#edit-name"),
-    editDescription: profileForm.querySelector("#edit-description"),
-    profileUserName: document.querySelector(".profile__text_type_username"),
-    profileDescription: document.querySelector(".profile__text_type_description")
-};
-
-const newPostSelectors = {
-    newPostModal: document.querySelector("#new-post-modal"),
-    newPostButton: document.querySelector(".profile__new-post"),
-    newPostImageLink: newPostForm.querySelector("#new-post-image-link"),
-    newPostCaption: newPostForm.querySelector("#new-post-caption")
-};
-
-const fullImageSelectors = {
-    fullImageModal: document.querySelector("#full-image-modal"),
-    fullImageImgElement: document.querySelector("#modal__full-image-img"),
-    fullImageText: document.querySelector(".modal__full-image-text")
-};
-
-const handleEscapeClose = (evt) => {
-    if (evt.key === "Escape") {
-        const openModal = document.querySelector(".modal.modal_opened");
-        if (openModal) {
-            toggleModal(openModal);
-            document.activeElement.blur();
-        }
-    }
-};
-
-const handleMouseClickClose = (evt) => {
-    const openModal = document.querySelector(".modal.modal_opened");
-    if (openModal && evt.target === openModal.children[0]) {
-        toggleModal(openModal);
-    }
-};
-
-const addModalListeners = () => {
-    document.addEventListener("keydown", handleEscapeClose);
-    document.addEventListener("click", handleMouseClickClose);
-};
-
-const removeModalListeners = () => {
-    document.removeEventListener("keydown", handleEscapeClose);
-    document.removeEventListener("click", handleMouseClickClose);
-};
-
-function toggleModal(modal) {
-    modal.classList.toggle("modal_opened");
-    const isOpened = modal.classList.contains("modal_opened");
-    isOpened ? addModalListeners() : removeModalListeners();
-}
-
-function fillEditFormFields() {
-    profileSelectors.editName.value =
-        profileSelectors.profileUserName.textContent.trim();
-    profileSelectors.editDescription.value =
-        profileSelectors.profileDescription.textContent.trim();
-}
-
-function setProfileModalListeners() {
-    profileSelectors.editButton.addEventListener("click", () => {
-        resetValidation(profileForm);
-        fillEditFormFields();
-        toggleModal(profileSelectors.editModal);
-    });
-}
-
-function updateUserInformation() {
-    profileForm.addEventListener("submit", (evt) => {
-        profileSelectors.profileUserName.textContent =
-            profileSelectors.editName.value;
-        profileSelectors.profileDescription.textContent =
-            profileSelectors.editDescription.value;
-        toggleModal(profileSelectors.editModal);
+    submit: (popup, evt, inputs) => {
         evt.preventDefault();
-    });
-}
+        constants.profileSelectors[0].textContent = inputs[0];
+        constants.profileSelectors[1].textContent = inputs[1];
+        popup.togglePopup();
+    }
+});
+updateProfileForm.setEventListeners();
+document.querySelector(".profile__edit-profile").addEventListener("click", () => updateProfileForm.togglePopup());
+new FormValidation(updateProfileForm.getPopupForm(), constants.formSelectors).enableValidation();
 
-function renderCard(data) {
-    photoList.prepend(getCardElement(data));
-}
-
-function openFullImage(data) {
-    fullImageSelectors.fullImageImgElement.src = "";
-    fullImageSelectors.fullImageImgElement.alt = "";
-    fullImageSelectors.fullImageText.textContent = "";
-    toggleModal(fullImageSelectors.fullImageModal);
-    fullImageSelectors.fullImageImgElement.src = data.src;
-    fullImageSelectors.fullImageImgElement.alt = data.alt;
-    fullImageSelectors.fullImageText.textContent = data.name;
-}
-
-function getCardElement(data) {
-    const cardElement = document
-        .querySelector("#card-template")
-        .content.querySelector(".photos__list-item")
-        .cloneNode(true);
-    cardElement.querySelector(".card__text").textContent = data.name;
-    const cardPhoto = cardElement.querySelector(".card__image");
-    cardPhoto.src = data.src;
-    cardPhoto.alt = data.alt;
-
-    const cardElementLike = cardElement.querySelector(".card__like-button");
-    cardElementLike.addEventListener("click", () =>
-        cardElementLike.classList.toggle("card__like-button_active"),
-    );
-    const cardElementDelete = cardElement.querySelector(".card__delete-button");
-    cardElementDelete.addEventListener("click", () => {
-        cardElement.remove();
-    });
-
-    const cardElementFullImageButton = cardElement.querySelector(
-        ".card__full-image-button",
-    );
-    cardElementFullImageButton.addEventListener("click", () =>
-        openFullImage(data),
-    );
-
-    return cardElement;
-}
-
-function addCards() {
-    initialCards.reverse().forEach((card) => renderCard(card));
-}
-
-function setNewPostListeners() {
-    newPostSelectors.newPostButton.addEventListener("click", () => {
-        resetValidation(newPostForm);
-        toggleModal(newPostSelectors.newPostModal);
-    });
-
-    newPostForm.addEventListener("submit", () => {
-        renderCard({
-            name: newPostSelectors.newPostCaption.value,
-            src: newPostSelectors.newPostImageLink.value,
-            alt: newPostSelectors.newPostCaption.value
+const newPostForm = new PopupWithForm(constants.newPostSelectors.newPostPopup, {
+    fill: () => {return ""},
+    submit: (popup, evt, inputs) => {
+        evt.preventDefault();
+        cardList.addItem({
+            src: inputs[0],
+            name: inputs[1],
+            alt: inputs[1]
                 .toLocaleLowerCase()
                 .replace(" ", "_"),
         });
-        newPostForm.reset();
-        disableButton(newPostForm);
-        toggleModal(newPostSelectors.newPostModal);
-    });
-}
-
-function setCloseButtonListeners() {
-    const closeButtons = document.querySelectorAll(".modal__close");
-
-    closeButtons.forEach((button) => {
-        const modal = button.closest(".modal");
-        button.addEventListener("click", () => toggleModal(modal));
-    });
-}
-
-setProfileModalListeners();
-setNewPostListeners();
-setCloseButtonListeners();
-updateUserInformation();
-addCards();
+        popup.togglePopup();
+    }
+});
+newPostForm.setEventListeners();
+document.querySelector(".profile__new-post").addEventListener("click", () => newPostForm.togglePopup());
+new FormValidation(newPostForm.getPopupForm(), constants.formSelectors).enableValidation();
