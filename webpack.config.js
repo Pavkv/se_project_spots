94f90075233f55bee48f1b18309e7b2ea7841aaa
Pavel Zobov <pavelzobov123@gmail.com>
2025-03-09 13:01:40 -0400

    feay: Add a package to deploy spots to the github pages

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
+            filename: "index.html",
             favicon: "./src/images/favicon.ico",
         }),
         new CleanWebpackPlugin(),
